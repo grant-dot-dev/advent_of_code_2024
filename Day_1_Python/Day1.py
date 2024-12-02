0a0c963bda6f2d36ac8e07eb5b0337704703cb52
grant-dot-dev <12433404+grant-dot-dev@users.noreply.github.com>
2024-12-02 20:41:45 +0000

    Day1 Python part 2 complete

--- Day_1_Python/Day1.py
+++ Day_1_Python/Day1.py
@@ -1,7 +1,16 @@
-def get_steps(list1, list2):
+from collections import Counter
+
+
+def part_1(list1, list2):
     totals = [abs(value - partner_item)
               for value, partner_item in zip(list1, list2)]
     print("Sum Total:", sum(totals))
+
+
+def part_2(list1, list2):
+    counts = Counter(list2)
+    total_sum = sum(number * counts[number] for number in list1)
+    print(f"Sum of counts: {total_sum}")
 
 
 def read_input(file_name):
@@ -10,6 +19,7 @@ def read_input(file_name):
     return sorted(list1), sorted(list2)
 
 
-list1, list2 = read_input("input.txt")
+list1, list2 = read_input("./input.txt")
 
-get_steps(list1, list2)
+part_1(list1, list2)
+part_2(list1, list2)
