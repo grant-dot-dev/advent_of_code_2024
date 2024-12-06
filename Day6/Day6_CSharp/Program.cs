f692e92fbe78e711902d75f80262038ccc87b190
grant-dot-dev <12433404+grant-dot-dev@users.noreply.github.com>
2024-12-06 22:01:03 +0000

    Day 6 - Guard Gallivant

--- Day6/Day6_CSharp/Program.cs
+++ Day6/Day6_CSharp/Program.cs
@@ -1,7 +1,14 @@
-﻿using Day6_CSharp;
+﻿using System.Diagnostics;
+using Day6_CSharp;
 
 var inputData = File.ReadAllLines("input1.txt");
 
-
+Stopwatch sw = new Stopwatch();
+sw.Start();
 Console.WriteLine($"Part 1 = {new Part1().Solve(inputData)}");
+Console.WriteLine($"Part 1 => {sw.Elapsed}ms");
+
 Console.WriteLine($"Part 2 = {new Part2().Solve(inputData)}");
+Console.WriteLine($"Part 2 => {sw.Elapsed}ms");
+
+sw.Stop();
